7f46a2b51467807c9af18072f77e913ce50a9c04
Sean M <sean.maddox89@yahoo.com>
2022-07-20 13:45:12 -0700

    server and routes wip

--- Develop/server.js
+++ Develop/server.js
@@ -2,16 +2,11 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('./api', apiRoutes);
-app.use('/', htmlRoutes);
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
 
